4d6e04d230c1cbd4d2ad4405e2eacba38e5f7a62
KaushalPandit1 <jck1445944@gmail.com>
2026-09-01 16:52:37 +0530

    s

--- supabase-config.js
+++ supabase-config.js
@@ -6,4 +6,4 @@
    ============================================================ */
 window.SSD_SUPABASE_URL = 'https://mwfinzhnitdeymakryej.supabase.co';
 window.SSD_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13ZmluemhuaXRkZXltYWtyeWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1OTk2MjIsImV4cCI6MjA5OTE3NTYyMn0.CpatNjgW_EvPzMGTr9MNnJysBv1w9UY4JrNDTx4CGj0';
-window.SSD_MEDIA_BUCKET = 'shri-sai-dwar-media';
+window.SSD_MEDIA_BUCKET = 'Shri-sai-dwar-media';
